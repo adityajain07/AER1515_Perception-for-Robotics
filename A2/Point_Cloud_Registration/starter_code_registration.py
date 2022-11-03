af28bbaf3f5af90a98c272bfef7011dbfefd287c
Aditya Jain <aditya14129@iiitd.ac.in>
2022-11-02 21:31:52 -0400

    registration code written

--- A2/Point_Cloud_Registration/starter_code_registration.py
+++ A2/Point_Cloud_Registration/starter_code_registration.py
@@ -1,3 +1,4 @@
+from tkinter import N
 import numpy as np
 from matplotlib import pyplot as plt
 import pandas as pd
@@ -16,7 +17,23 @@ def nearest_search(pcd_source, pcd_target):
     corr_target = []
     corr_source = []
     ec_dist_mean = 0
-    return corr_source, corr_target, ec_dist_mean
+    ec_dist_list = []
+
+    for pt_source in pcd_source:
+        corr_source.append(pt_source)
+        min_ec_dist = 1e6
+        min_pt_target = None
+        for pt_target in pcd_target:
+            ec_dist = np.linalg.norm(pt_target - pt_source)
+            if ec_dist < min_ec_dist:
+                min_ec_dist   = ec_dist
+                min_pt_target = pt_target
+        ec_dist_list.append(min_ec_dist)
+        corr_target.append(min_pt_target)
+
+    ec_dist_mean = sum(ec_dist_list)/len(ec_dist_list)
+
+    return np.array(corr_source), np.array(corr_target), ec_dist_mean
 
 
 def estimate_pose(corr_source, corr_target):
@@ -26,16 +43,65 @@ def estimate_pose(corr_source, corr_target):
     translation_x = 0
     translation_y = 0
     translation_z = 0
+
+    corr_source = np.array(corr_source)
+    corr_target = np.array(corr_target)
+    N           = len(corr_source)
+
+    # get centroids for source and target cluster
+    p_s = np.mean(corr_source, axis=0)     # centroid of source
+    p_t = np.mean(corr_target, axis=0)     # centroid of target
+
+    # compute outer product matrix
+    W_ts = 0
+    for i in range(N):
+        W_ts += ((corr_target[i] - p_t).reshape(3,1))@((corr_source[i] - p_s).reshape(1,3))
+    W_ts = W_ts/N
+
+    # compute SVD
+    V, _, U_T = np.linalg.svd(W_ts)
+    det_V = np.linalg.det(V)
+    det_U = np.linalg.det(U_T.T)
+    temp  = np.array([[1, 0, 0], 
+                      [0, 1, 0], 
+                      [0, 0, det_U*det_V]])
+    C_ts  = V@temp@U_T
+    r_s_ts = -(C_ts.T)@p_t + p_s
+
+    # final translation
+    r = -C_ts@r_s_ts
+    translation_x = r[0]
+    translation_y = r[1]
+    translation_z = r[2]
+
+    # final pose
+    temp = np.hstack((C_ts, r.reshape(3,1)))
+    pose = np.vstack((temp, np.array([0, 0, 0, 1])))
+
     return pose, translation_x, translation_y, translation_z
 
 
-def icp(pcd_source, pcd_target):
+def icp(pcd_source, pcd_target, iter=30):
     # TODO: Put all together, implement the ICP algorithm
     # TODO: Use your implemented functions "nearest_search" and "estimate_pose"
     # TODO: Run 30 iterations
     # TODO: Show the plot of mean euclidean distance (from function "nearest_search") for each iteration
     # TODO: Show the plot of pose translation (from function "estimate_pose") for each iteration
     pose = np.identity(4)
+    cur_source = pcd_source
+    
+    for i in range(iter):
+        corr_source, corr_target, ec_dist_mean = nearest_search(cur_source, pcd_target)        
+        pose, translation_x, translation_y, translation_z = estimate_pose(corr_source, corr_target)
+
+        # transform the source points according to the calculated pose
+        pts              = np.vstack([np.transpose(corr_source), np.ones(len(corr_source))])
+        cloud_registered = np.matmul(pose, pts)
+        cloud_registered = np.transpose(cloud_registered[0:3, :])
+        cur_source       = cloud_registered
+
+        print(f'Mean euclidean distance for iteration {i+1} is {ec_dist_mean}')
+
     return pose
 
 
@@ -70,6 +136,7 @@ def main():
     ##########################################################################################################
     for i in range(2):
         # Load data
+        print('Plotting for: ', train_file[i])
         path_source = './training/' + train_file[i] + '_source.csv'
         path_target = './training/' + train_file[i] + '_target.csv'
         pcd_source = load_point_cloud(path_source)
@@ -77,22 +144,23 @@ def main():
         gt_pose_i = GT_poses[i]
 
         # Visualize the point clouds before the registration
-        ax = plt.axes(projection='3d')
-        ax.scatter3D(pcd_source[:,0], pcd_source[:,1], pcd_source[:,2], cmap='Greens')
-        ax.scatter3D(pcd_target[:,0], pcd_target[:,1], pcd_target[:,2], cmap='Reds')
-        plt.legend(["Source Point Cloud" , "Target Point Cloud"])
-        ax.set_title('Point Clouds Before Registration')
-        plt.show()
+        # ax = plt.axes(projection='3d')
+        # ax.scatter3D(pcd_source[:,0], pcd_source[:,1], pcd_source[:,2], cmap='Greens')
+        # ax.scatter3D(pcd_target[:,0], pcd_target[:,1], pcd_target[:,2], cmap='Reds')
+        # plt.legend(["Source Point Cloud" , "Target Point Cloud"])
+        # ax.set_title('Point Clouds Before Registration')
+        # plt.show()
 
 
 
         # TODO: Use your implemented ICP algorithm to get the estimated 6D pose (from source to target point cloud)
-        # pose = icp(pcd_source, pcd_target)
+        pose = icp(pcd_source, pcd_target)
 
         # Transform the point cloud
         # TODO: Replace the ground truth pose with your computed pose and transform the source point cloud
         pts = np.vstack([np.transpose(pcd_source), np.ones(len(pcd_source))])
-        cloud_registered = np.matmul(gt_pose_i, pts)
+        # cloud_registered = np.matmul(gt_pose_i, pts)
+        cloud_registered = np.matmul(pose, pts)
         cloud_registered = np.transpose(cloud_registered[0:3, :])
 
         # TODO: Evaluate the rotation and translation error of your estimated 6D pose with the ground truth pose
